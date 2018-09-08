0ae0f0963d605353b5b086cb6301a6c817ef00f1
Artemy <artidnb@icloud.com>
2018-09-08 12:22:08 +0300

    pixel perfect 3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 var link = document.querySelector(".form-feedback");
 var popup = document.querySelector(".modal-feedback");
+var overlay = document.querySelector(".overlay");
 var close = popup.querySelector(".modal-close");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-feedback-show");
+  overlay.classList.add("overlay-show");
   login.focus();
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-feedback-show");
+  overlay.classList.remove("overlay-show");
 })
