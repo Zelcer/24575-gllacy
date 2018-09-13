e08ee726c50a1f21bb40254b0b373395a9e3c550
Artemy <artidnb@icloud.com>
2018-09-13 10:47:20 +0300

    Допуск к защите 9

--- js/script.js
+++ js/script.js
@@ -3,9 +3,30 @@ var popup = document.querySelector(".modal-feedback");
 var overlay = document.querySelector(".overlay");
 var close = popup.querySelector(".modal-close");
 
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal-feedback-show");
+
+    if (storage) {
+      name.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
+
     overlay.classList.add("overlay-show");
 });
 
@@ -13,14 +34,35 @@ close.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.remove("modal-feedback-show");
     overlay.classList.remove("overlay-show");
-})
+});
 
+form.addEventListener("submit", function(evt) {
+  if (!name.value || !email.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести имя и email");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", name.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-feedback-show")) {
+      popup.classList.remove("modal-feedback-show");
+    }
+  }
+});
+
+// Yandex Map
 ymaps.ready(init);
 
 function init() {
     var myMap = new ymaps.Map("map", {
-        center: [59.939, 30.326332335998537],
-        zoom: 17
+        center: [59.9393, 30.329532335998537],
+        zoom: 16
     });
 
     var myPlacemark = new ymaps.Placemark([59.938744116548456, 30.323097415344236], {
